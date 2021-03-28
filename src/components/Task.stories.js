9a908551bdf3d2398ca7e2b2c0112fa546719718
weirdyang <wy4786@gmail.com>
2021-03-29 01:48:17 +0800

    testing edge case

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,6 +1,6 @@
+import { action } from '@storybook/addon-actions';
 import Task from './Task';
 
-import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Task',
@@ -44,5 +44,15 @@ Archived.args = {
   task: {
     ...Default.args.task,
     state: 'TASK_ARCHIVED',
+  },
+};
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: longTitleString,
   },
 };
