5102c6b93c702ee1889de866b4c69c4124006e76
weirdyang <wy4786@gmail.com>
2021-03-29 01:15:18 +0800

    compositec component example complete

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: ["error", "always"]
   },
   overrides: [
     {
---
+++ tests/unit/TaskList.spec.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+//👇 Our story imported here
+import { WithPinnedTasks } from '../../src/components/TaskList.stories';
+import TaskList from '../../src/components/TaskList.vue';
+
+
+
+it('renders pinned tasks at the start of the list', () => {
+  // render Tasklist
+  const Constructor = Vue.extend(TaskList);
+  const vm = new Constructor({
+    //👇 Story's args used with our test
+    propsData: WithPinnedTasks.args,
+  }).$mount();
+  const firstTaskPinned = vm.$el.querySelector('.list-item:nth-child(1).TASK_PINNED');
+
+  // We expect the pinned task to be rendered first, not at the end
+  expect(firstTaskPinned).not.toBe(null);
+});
